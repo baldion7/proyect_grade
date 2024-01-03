6ef087c691b144ef6189fdc1bdde357bce5c39ad
Kevin Jose Quiñones Baldion <73363421+baldion7@users.noreply.github.com>
2024-01-03 16:11:36 -0600

    Update ViewsRoute.js

--- routes/ViewsRoute.js
+++ routes/ViewsRoute.js
@@ -1,15 +1,14 @@
 import express from "express";
-import {adminOnly, verifyUser,requireLogin,AtchUser} from "../middleware/AuthUser.js";
-import {AdminView, UserView,RegisterView,LoginView,ErrorView,prueba,EquipmentView,CampusView,BuldingView,ClassificationView,TypesEquipmentView,AreaView,SpaceView,PersonnelResponsibleView} from "../controllers/Views.js";
+import {AtchUser} from "../middleware/AuthUser.js";
+import {AdminView, UserView,RegisterView,LoginView,ErrorView,EquipmentView,CampusView,BuldingView,ClassificationView,TypesEquipmentView,AreaView,SpaceView,PersonnelResponsibleView} from "../controllers/Views.js";
 const router = express.Router();
-router.get('/admin',verifyUser,adminOnly,AdminView);
-router.get('/user',verifyUser,UserView);
+router.get('/admin',AdminView);
+router.get('/user',UserView);
 router.get('/register',RegisterView);
-router.get('/login',LoginView,adminOnly);
+router.get('/login',LoginView);
 router.get('/',LoginView);
 router.get('/error',ErrorView)
 router.delete('/AtchUser',AtchUser);
-router.get('/usuarios',prueba)
 router.get('/equipment',EquipmentView)
 router.get('/campus',CampusView)
 router.post('/bulding',BuldingView)
@@ -18,4 +17,5 @@ router.post('/area',AreaView)
 router.get('/classification',ClassificationView)
 router.post('/space',SpaceView)
 router.get('/personnelresponsible',PersonnelResponsibleView)
+//ajustes de seguridad
 export default router;
