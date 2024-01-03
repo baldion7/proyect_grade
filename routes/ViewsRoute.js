aa82be9fcd5af9aa3b779e74dab15eab66db7db3
Kevin Jose Quiñones Baldion <73363421+baldion7@users.noreply.github.com>
2024-01-03 16:14:05 -0600

    Update ViewsRoute.js

--- routes/ViewsRoute.js
+++ routes/ViewsRoute.js
@@ -1,21 +1,21 @@
 import express from "express";
-import {adminOnly, verifyUser,requireLogin,AtchUser} from "../middleware/AuthUser.js";
-import {AdminView, UserView,RegisterView,LoginView,ErrorView,prueba,EquipmentView,CampusView,BuldingView,ClassificationView,TypesEquipmentView,AreaView,SpaceView,PersonnelResponsibleView} from "../controllers/Views.js";
+import {AtchUser} from "../middleware/AuthUser.js";
+import {AdminView, UserView,RegisterView,LoginView,ErrorView,EquipmentView,CampusView,BuldingView,ClassificationView,TypesEquipmentView,AreaView,SpaceView,PersonnelResponsibleView} from "../controllers/Views.js";
 const router = express.Router();
-router.get('/admin',verifyUser,adminOnly,AdminView);
-router.get('/user',verifyUser,UserView);
+router.get('/admin',AdminView);
+router.get('/user',UserView);
 router.get('/register',RegisterView);
-router.get('/login',LoginView,adminOnly);
+router.get('/login',LoginView);
 router.get('/',LoginView);
 router.get('/error',ErrorView)
 router.delete('/AtchUser',AtchUser);
-router.get('/usuarios',verifyUser,prueba)
-router.get('/equipment',verifyUser,EquipmentView)
-router.get('/campus',verifyUser,CampusView)
-router.post('/bulding',verifyUser,BuldingView)
-router.post('/typesequipment',verifyUser,TypesEquipmentView)
-router.post('/area',verifyUser,AreaView)
-router.get('/classification',verifyUser,ClassificationView)
-router.post('/space',verifyUser,SpaceView)
-router.get('/personnelresponsible',verifyUser,PersonnelResponsibleView)
+router.get('/equipment',EquipmentView)
+router.get('/campus',CampusView)
+router.post('/bulding',BuldingView)
+router.post('/typesequipment',TypesEquipmentView)
+router.post('/area',AreaView)
+router.get('/classification',ClassificationView)
+router.post('/space',SpaceView)
+router.get('/personnelresponsible',PersonnelResponsibleView)
+//ajustes de seguridad
 export default router;
