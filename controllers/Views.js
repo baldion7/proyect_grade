192aebcd6992d8956208bb9b7ef9fd559623ba0c
Kevin Jose Quiñones Baldion <73363421+baldion7@users.noreply.github.com>
2024-01-06 21:02:49 -0600

    Update Views.js

--- controllers/Views.js
+++ controllers/Views.js
@@ -1,20 +1,17 @@
 export const AdminView=(req,res)=>{
-    res.render('pages/ViewUser', { title: 'Mi aplicación Node.js', perfil:req.session.name});
+    res.render('pages/ViewUser', { title: 'Usuarios', perfil:"j"});
 }
 export const CampusView=(req,res)=>{
-    res.render('pages/ViewCampus', { title: 'Mi aplicación Node.js', perfil:req.session.name});
-}
-export const prueba=(req,res)=>{
-    res.render('pages/ViewEquipment', { title: 'Mi aplicación Node.js', perfil:req.session.name});
+    res.render('pages/ViewCampus', { title: 'Regionales', perfil:"j"});
 }
 export const UserView=(req,res)=>{
-    res.render('pages/user', { title: 'Mi aplicación Node.js' });
+    res.render('pages/ViewUser', { title: 'Usuario', perfil:"j" });
 }
 export const PersonnelResponsibleView=(req,res)=>{
-    res.render('pages/ViewPersonnelResponsible', { title: 'Mi aplicación Node.js', perfil:req.session.name});
+    res.render('pages/ViewPersonnelResponsible', { title: 'Responsable', perfil:"j"});
 }
 export const RegisterView=(req,res)=>{
-    res.render('pages/register', { title: 'Mi aplicación Node.js' });
+    res.render('pages/register', { title: 'Registro' });
 }
 export const LoginView=(req,res)=>{
     res.render('pages/login');
@@ -26,25 +23,25 @@ export const ResetPasswordView=(req,res)=>{
     res.render('pages/forgot-password');
 }
 export const EquipmentView=(req,res)=>{
-    res.render('pages/ViewEquipment',{ title: 'Mi aplicación Node.js', perfil:req.session.name});
+    res.render('pages/ViewEquipment',{ title: 'Equipos', perfil:"j"});
 }
 export const BuldingView=(req,res)=>{
     const {ID} = req.body;
-    res.render('pages/ViewBuilding', { title: 'Mi aplicación Node.js', perfil:req.session.name, id:ID});
+    res.render('pages/ViewBuilding', { title: 'Edificios', perfil:"j", id:1});
 }
 export const TypesEquipmentView=(req,res)=>{
     const {ID} = req.body;
-    res.render('pages/ViewTypesEquipment', { title: 'Mi aplicación Node.js', perfil:req.session.name, id:ID});
+    res.render('pages/ViewTypesEquipment', { title: 'Categorías', perfil:"j", id:1});
 }
 
 export const AreaView=(req,res)=>{
     const {ID,Name} = req.body;
-    res.render('pages/ViewArea', { title: 'Mi aplicación Node.js', perfil:req.session.name, id:ID,name:Name});
+    res.render('pages/ViewArea', { title: 'Áreas', perfil:req.session.name, id:ID,name:Name});
 }
 export const SpaceView=(req,res)=>{
     const {ID,Name} = req.body;
-    res.render('pages/ViewSpace', { title: 'Mi aplicación Node.js', perfil:req.session.name, id:ID,name:Name});
+    res.render('pages/ViewSpace', { title: 'Espacios', perfil:req.session.name, id:ID,name:Name});
 }
 export const ClassificationView=(req,res)=>{
-    res.render('pages/ViewClassification', { title: 'Mi aplicación Node.js', perfil:req.session.name});
+    res.render('pages/ViewClassification', { title: 'Clasificación', perfil:req.session.name});
 }
